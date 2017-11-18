af08d21a1d31d2d7c788613e13562b2d2fc71826
John Ramsden <johnramsden@riseup.net>
2017-11-17 23:48:29 -0800

    Setup list command options

--- zedenv/cli/list.py
+++ zedenv/cli/list.py
@@ -2,12 +2,39 @@
 
 import click
 
+import zedenv.lib.zfs.command as zfs_command
+import zedenv.lib.zfs.utility as zfs_utility
+import zedenv.lib.zfs.linux as zfs_linux
+
+import zedenv.lib.boot_environment as be
+
+from zedenv.lib.logger import ZELogger
+
+
 
 @click.command(name="list",
                help="List all boot environments.")
 @click.option('--verbose', '-v',
               is_flag=True,
               help="Print verbose output.")
-def cli(boot_environment, verbose):
-    if verbose:
-        click.echo("Listing Boot Environments.")
+@click.option('--all', '-a',
+              is_flag=True,
+              help="Display all datasets.")
+@click.option('--spaceused', '-D',
+              is_flag=True,
+              help="Display the full space usage for each boot environment.")
+@click.option('--scripting', '-H',
+              is_flag=True,
+              help="Scripting output.")
+@click.option('--snapshots', '-s',
+              is_flag=True,
+              help="Display snapshots.")
+def cli(verbose, all, spaceused, scripting, snapshots):
+
+    parent_dataset = be.get_root()
+    root_dataset = zfs_linux.mount_dataset("/")
+
+    ZELogger.verbose_log({
+        "level":   "INFO", "message": "Listing Boot Environments:\n"
+    }, verbose)
+
