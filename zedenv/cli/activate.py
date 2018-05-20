johnramsden/zedenv

commit f9b5fff6982bbc156295b5e0bd00dc9b79a312a8

Move functions unrelated to 'check' to 'be' module

zedenv/cli/activate.py
@@ -126,7 +126,7 @@ def activate_boot_environment(be_requested: str,
         except RuntimeError as e:
             ZELogger.log({
                 "level": "EXCEPTION",
-                "message": f"Failed unmounting dataset {boot_environment}\n{e}\n"
+                "message": f"Failed unmounting dataset {be_requested}\n{e}\n"
             }, exit_on_error=True)
 
         mount_and_modify_dataset(be_requested,
@@ -193,7 +193,8 @@ def zedenv_activate(boot_environment: str,
             "message": f"Boot environment {boot_environment} exists'\n"
         }, verbose)
 
-    if zedenv.lib.be.is_current_boot_environment(boot_environment):
+    if zedenv.lib.be.is_active_boot_environment(be_requested,
+                                                zedenv.lib.be.dataset_pool(be_requested)):
         ZELogger.verbose_log({
             "level": "INFO",
             "message": f"Boot Environment {boot_environment} is already active.\n"
@@ -202,46 +203,46 @@ def zedenv_activate(boot_environment: str,
         dataset_mountpoint = pyzfscmds.system.agnostic.dataset_mountpoint(be_requested)
         activate_boot_environment(be_requested, dataset_mountpoint, verbose, bootloader_plugin)
 
-    be_child_datasets = None
-    try:
-        be_child_datasets = pyzfscmds.cmd.zfs_list(boot_environment_root,
-                                                   recursive=True,
-                                                   columns=["name"],
-                                                   zfs_types=["filesystem"])
-    except RuntimeError as e:
-        ZELogger.log({
-            "level": "EXCEPTION",
-            "message": f"Failed to list datasets under {boot_environment_root}\n{e}\n"
-        }, exit_on_error=True)
+        be_child_datasets = None
+        try:
+            be_child_datasets = pyzfscmds.cmd.zfs_list(boot_environment_root,
+                                                       recursive=True,
+                                                       columns=["name"],
+                                                       zfs_types=["filesystem"])
+        except RuntimeError as e:
+            ZELogger.log({
+                "level": "EXCEPTION",
+                "message": f"Failed to list datasets under {boot_environment_root}\n{e}\n"
+            }, exit_on_error=True)
 
-    be_child_datasets_list = [line for line in be_child_datasets.splitlines()]
-    disable_children_automount(be_child_datasets_list,
-                               be_requested,
-                               boot_environment_root,
-                               verbose)
+        be_child_datasets_list = [line for line in be_child_datasets.splitlines()]
+        disable_children_automount(be_child_datasets_list,
+                                   be_requested,
+                                   boot_environment_root,
+                                   verbose)
 
-    # TODO: Should this be noauto?
-    canmount_setting = "canmount=on"
-    for ds in be_child_datasets_list:
-        if be_requested == ds:
-            try:
-                pyzfscmds.cmd.zfs_set(ds, canmount_setting)
-            except RuntimeError:
-                ZELogger.log({
-                    "level": "EXCEPTION",
-                    "message": f"Failed to set {canmount_setting} for {ds}\n{e}\n"
-                }, exit_on_error=True)
-
-            if pyzfscmds.utility.is_clone(ds):
+        # TODO: Should this be noauto?
+        canmount_setting = "canmount=on"
+        for ds in be_child_datasets_list:
+            if be_requested == ds:
                 try:
-                    pyzfscmds.cmd.zfs_promote(ds)
+                    pyzfscmds.cmd.zfs_set(ds, canmount_setting)
                 except RuntimeError:
                     ZELogger.log({
                         "level": "EXCEPTION",
-                        "message": f"Failed to promote BE {ds}\n{e}\n"
+                        "message": f"Failed to set {canmount_setting} for {ds}\n{e}\n"
                     }, exit_on_error=True)
-                ZELogger.verbose_log(
-                    {"level": "INFO", "message": f"Promoted {ds}.\n"}, verbose)
+
+                if pyzfscmds.utility.is_clone(ds):
+                    try:
+                        pyzfscmds.cmd.zfs_promote(ds)
+                    except RuntimeError:
+                        ZELogger.log({
+                            "level": "EXCEPTION",
+                            "message": f"Failed to promote BE {ds}\n{e}\n"
+                        }, exit_on_error=True)
+                    ZELogger.verbose_log(
+                        {"level": "INFO", "message": f"Promoted {ds}.\n"}, verbose)
 
 
 @click.command(name="activate",

zedenv/cli/destroy.py
@@ -15,6 +15,7 @@ from zedenv.lib.logger import ZELogger
 
 def zedenv_destroy(boot_environment: str,
                    be_root: str,
+                   root_dataset: str,
                    verbose: Optional[bool],
                    unmount: Optional[bool]):
     """
@@ -23,6 +24,16 @@ def zedenv_destroy(boot_environment: str,
     ZELogger.verbose_log({
         "level": "INFO", "message": f"Destroying Boot Environment: '{boot_environment}'\n"
     }, verbose)
+
+    boot_environment_dataset = f"{be_root}/{boot_environment}"
+
+    if zedenv.lib.be.is_active_boot_environment(
+            boot_environment_dataset,
+            zedenv.lib.be.dataset_pool(boot_environment_dataset)):
+        ZELogger.log({
+            "level": "EXCEPTION",
+            "message": f"Cannot destroy active boot environment '{boot_environment}'."
+        }, exit_on_error=True)
 
 
 @click.command(name="destroy",
@@ -43,4 +54,8 @@ def cli(boot_environment: str,
     except RuntimeError as err:
         sys.exit(err)
 
-    zedenv_destroy(boot_environment, zedenv.lib.be.root(), verbose, unmount)
+    zedenv_destroy(boot_environment,
+                   zedenv.lib.be.root(),
+                   pyzfscmds.system.agnostic.mountpoint_dataset("/"),
+                   verbose,
+                   unmount)

tests/cli/test_activate.py
@@ -8,6 +8,7 @@ import zedenv.cli.create
 import pyzfscmds.utility
 
 require_root_dataset = pytest.mark.require_root_dataset
+require_zfs_version = pytest.mark.require_zfs_version
 
 
 def create_new_boot_environment(root_dataset):
@@ -23,6 +24,7 @@ def create_new_boot_environment(root_dataset):
     return f"{parent_dataset}/{boot_environment}"
 
 
+@require_zfs_version
 @require_root_dataset
 def test_boot_environment_activated(root_dataset):
 

tests/cli/test_destroy.py
@@ -8,9 +8,11 @@ import pyzfscmds.utility as zfs_utility
 import zedenv.cli.destroy
 import zedenv.cli.create
 import zedenv.lib.check
+import zedenv.cli.activate
 
 require_root_dataset = pytest.mark.require_root_dataset
 require_unsafe = pytest.mark.require_unsafe
+require_zfs_version = pytest.mark.require_zfs_version
 
 
 @require_root_dataset
@@ -25,17 +27,42 @@ def created_boot_environment(root_dataset):
     zedenv.cli.create.zedenv_create(parent_dataset, root_dataset,
                                     boot_environment, verbose, existing)
 
-    return f"{parent_dataset}/{boot_environment}"
+    return boot_environment
 
 
 @require_unsafe
 @require_root_dataset
+@require_zfs_version
 def test_boot_environment_destroyed(root_dataset, created_boot_environment):
     parent_dataset = zfs_utility.dataset_parent(root_dataset)
 
     verbose = True
     unmount = False
 
-    zedenv.cli.destroy.zedenv_destroy(created_boot_environment, parent_dataset, verbose, unmount)
+    zedenv.cli.destroy.zedenv_destroy(created_boot_environment,
+                                      parent_dataset,
+                                      root_dataset,
+                                      verbose,
+                                      unmount)
 
     # assert not zfs_utility.dataset_exists(f"{parent_dataset}/{created_boot_environment}")
+
+
+@require_unsafe
+@require_root_dataset
+@require_zfs_version
+def test_boot_environment_destroy_fails(root_dataset, created_boot_environment):
+    parent_dataset = zfs_utility.dataset_parent(root_dataset)
+
+    verbose = True
+    unmount = False
+
+    zedenv.cli.activate.zedenv_activate(created_boot_environment,
+                                        parent_dataset, verbose, None, False)
+
+    with pytest.raises(SystemExit):
+        zedenv.cli.destroy.zedenv_destroy(created_boot_environment,
+                                          parent_dataset,
+                                          root_dataset,
+                                          verbose,
+                                          unmount)

tests/test_startup.py
@@ -32,7 +32,7 @@ def set_bootfs_failure(request, zpool, root_dataset):
 @require_zpool
 def check_startup(zpool, set_bootfs_failure):
     with pytest.raises(RuntimeError):
-        zedenv.lib.check.startup_check_bootfs(zpool)
+        zedenv.lib.be.bootfs_for_pool(zpool)
 
 
 @require_zpool

zedenv/cli/list.py
@@ -49,7 +49,7 @@ def configure_boot_environment_list(be_root: str,
             if pyzfscmds.system.agnostic.mountpoint_dataset("/") == env[0]:
                 active = "N"
 
-            if zedenv.lib.check.bootfs_for_pool(
+            if zedenv.lib.be.bootfs_for_pool(
                     zedenv.lib.be.dataset_pool(env[0])) == env[0]:
                 active += "R"
 

zedenv/lib/be.py
@@ -35,6 +35,23 @@ def size(boot_environment) -> int:
 """
 
 
+def bootfs_for_pool(zpool: str) -> str:
+    bootfs_list = None
+    try:
+        bootfs_list = pyzfscmds.cmd.zpool_get(pool=zpool,
+                                              scripting=True,
+                                              properties=["bootfs"],
+                                              columns=["value"])
+    except RuntimeError as err:
+        raise
+
+    bootfs = [i.split()[0] for i in bootfs_list.splitlines() if i.split()[0] != "-"]
+    if not bootfs:
+        raise RuntimeError("No bootfs has been set on zpool")
+
+    return bootfs[0]
+
+
 def properties(dataset, appended_properties: Optional[list]) -> list:
     dataset_properties = None
     try:
@@ -129,7 +146,11 @@ def is_current_boot_environment(boot_environment: str) -> bool:
     if be_root is None or not (root_dataset == "/".join([be_root, boot_environment])):
         return False
 
-    return zedenv.lib.check.startup_check_bootfs(mount_pool()) == root_dataset
+    return bootfs_for_pool(mount_pool()) == root_dataset
+
+
+def is_active_boot_environment(boot_environment_dataset: str, zpool: str) -> bool:
+    return bootfs_for_pool(zpool) == boot_environment_dataset
 
 
 def list_boot_environments(target: str, columns: list) -> list:

zedenv/lib/check.py
@@ -6,36 +6,7 @@ import pyzfscmds.check
 import pyzfscmds.cmd
 import pyzfscmds.system.agnostic
 
-
-def bootfs_for_pool(zpool: str) -> str:
-    bootfs_list = None
-    try:
-        bootfs_list = pyzfscmds.cmd.zpool_get(pool=zpool,
-                                              scripting=True,
-                                              properties=["bootfs"],
-                                              columns=["value"])
-    except RuntimeError as err:
-        raise
-
-    bootfs = [i.split()[0] for i in bootfs_list.splitlines() if i.split()[0] != "-"]
-    if not bootfs:
-        raise RuntimeError("No bootfs has been set on zpool")
-
-    return bootfs[0]
-
-
-def startup_check_bootfs(zpool: str) -> str:
-    """
-    Checks if system is supported and has a bootfs set
-    Returns bootfs for specified pool
-    """
-    bootfs = None
-    try:
-        bootfs = bootfs_for_pool(zpool)
-    except RuntimeError as err:
-        raise
-
-    return bootfs
+import zedenv.lib.be
 
 
 def startup_check():
@@ -46,7 +17,7 @@ def startup_check():
             f"System is not booting off a ZFS root dataset.\n{err}\n")
 
     try:
-        startup_check_bootfs(
+        zedenv.lib.be.bootfs_for_pool(
             pyzfscmds.system.agnostic.mountpoint_dataset("/").split("/")[0])
     except RuntimeError as err:
         raise RuntimeError(f"Couldn't get bootfs property of pool.\n{err}\n")

commit 0ad892f5bbcd3bf6c8ccb822f0e4d4f01c88bca9

Find promoted datasets for destroy
Fix bug introduced in f9b5fff6982bbc156295b5e0bd00dc9b79a312a8
where BEs were not being promoted if they were active

zedenv/cli/activate.py
@@ -229,25 +229,25 @@ def zedenv_activate(boot_environment: str,
         dataset_mountpoint = pyzfscmds.system.agnostic.dataset_mountpoint(be_requested)
         activate_boot_environment(be_requested, dataset_mountpoint, verbose, bootloader_plugin)
 
-        be_child_datasets = None
-        try:
-            be_child_datasets = pyzfscmds.cmd.zfs_list(boot_environment_root,
-                                                       recursive=True,
-                                                       columns=["name"],
-                                                       zfs_types=["filesystem"])
-        except RuntimeError as e:
-            ZELogger.log({
-                "level": "EXCEPTION",
-                "message": f"Failed to list datasets under {boot_environment_root}\n{e}\n"
-            }, exit_on_error=True)
+    be_child_datasets = None
+    try:
+        be_child_datasets = pyzfscmds.cmd.zfs_list(boot_environment_root,
+                                                   recursive=True,
+                                                   columns=["name"],
+                                                   zfs_types=["filesystem"])
+    except RuntimeError as e:
+        ZELogger.log({
+            "level": "EXCEPTION",
+            "message": f"Failed to list datasets under {boot_environment_root}\n{e}\n"
+        }, exit_on_error=True)
 
-        be_child_datasets_list = [line for line in be_child_datasets.splitlines()]
-        disable_children_automount(be_child_datasets_list,
-                                   be_requested,
-                                   boot_environment_root,
-                                   verbose)
+    be_child_datasets_list = [line for line in be_child_datasets.splitlines()]
+    disable_children_automount(be_child_datasets_list,
+                               be_requested,
+                               boot_environment_root,
+                               verbose)
 
-        apply_settings_to_child_datasets(be_child_datasets_list, be_requested, verbose)
+    apply_settings_to_child_datasets(be_child_datasets_list, be_requested, verbose)
 
 
 @click.command(name="activate",

zedenv/cli/destroy.py
@@ -1,5 +1,6 @@
 """List boot environments cli"""
 
+import re
 import sys
 from typing import Optional
 
@@ -14,6 +15,51 @@ import zedenv.lib.check
 from zedenv.lib.logger import ZELogger
 
 
+def get_promote_snapshots(be_pool: str, destroy_dataset: str) -> list:
+    """
+    Look for clone we need to promote because they're dependent on snapshots
+    """
+    promote_snaps = None
+    try:
+        promote_snaps = pyzfscmds.cmd.zfs_list(
+            be_pool, recursive=True,
+            columns=['name', 'origin'], zfs_types=['filesystem', 'snapshot', 'volume'])
+    except RuntimeError as e:
+        ZELogger.log({
+            "level": "EXCEPTION",
+            "message": f"Failed to list snapshots for promote in '{be_pool}'.\n{e}"
+        }, exit_on_error=True)
+
+    split_promote_snaps = zedenv.lib.be.split_zfs_output(promote_snaps)
+
+    ZELogger.verbose_log({
+        "level": "INFO",
+        "message": f"Found snapshots to promote:\n{split_promote_snaps}"
+    }, True)
+
+    target = re.compile(r'\b' + destroy_dataset + r'(@|/.*@).*' + r'\b')
+    return [ds[0] for ds in split_promote_snaps if target.match(ds[1])]
+
+
+def get_origin_snapshots(be_pool: str, destroy_dataset: str) -> list:
+    origin_all_snaps = None
+    try:
+        origin_all_snaps = pyzfscmds.cmd.zfs_list(
+            be_pool, recursive=True,
+            columns=['origin'], zfs_types=['filesystem', 'snapshot', 'volume'])
+    except RuntimeError as e:
+        ZELogger.log({
+            "level": "EXCEPTION",
+            "message": f"Failed to list origin snapshots for '{be_pool}'.\n{e}"
+        }, exit_on_error=True)
+
+    split_snaps = zedenv.lib.be.split_zfs_output(origin_all_snaps)
+
+    target = re.compile(r'\b' + destroy_dataset + r'(@|/.*@).*' + r'\b')
+
+    return [ds[1] for ds in split_snaps if target.match(ds[0])]
+
+
 def zedenv_destroy(target: str,
                    be_root: str,
                    root_dataset: str,
@@ -24,10 +70,10 @@ def zedenv_destroy(target: str,
     """
     Put actual function to be called in this separate function to allow easier testing.
     """
-    boot_environment_dataset = f"{be_root}/{target}"
-    ds_is_snapshot = pyzfscmds.utility.is_snapshot(boot_environment_dataset)
+    destroy_dataset = f"{be_root}/{target}"
+    ds_is_snapshot = pyzfscmds.utility.is_snapshot(destroy_dataset)
     be_pool = zedenv.lib.be.dataset_pool(
-        boot_environment_dataset,
+        destroy_dataset,
         zfs_type='filesystem' if not ds_is_snapshot else 'snapshot')
 
     if be_pool is None:
@@ -42,53 +88,65 @@ def zedenv_destroy(target: str,
             "message": f"Cannot destroy active boot environment '{target}'."
         }, exit_on_error=True)
 
+    if pyzfscmds.system.agnostic.dataset_mountpoint(destroy_dataset) == "/":
+        ZELogger.log({
+            "level": "EXCEPTION",
+            "message": f"Cannot destroy current root dataset environment '{target}'."
+        }, exit_on_error=True)
+
     if not noconfirm:
         click.confirm(f"Do you really want to destroy '{target}'?\n"
                       "This action will be permanent.\n\n"
-                      f"Destroy '{boot_environment_dataset}'?", abort=True)
+                      f"Destroy '{destroy_dataset}'?", abort=True)
         click.echo()
 
     if ds_is_snapshot:
         if not noop:
             try:
-                pyzfscmds.cmd.zfs_destroy_snapshot(boot_environment_dataset)
+                pyzfscmds.cmd.zfs_destroy_snapshot(destroy_dataset)
             except RuntimeError as e:
                 ZELogger.log({
                     "level": "EXCEPTION",
                     "message": f"Snapshot may be origin for other boot environment.\n{e}"
                 }, exit_on_error=True)
         ZELogger.verbose_log(
-            {"level": "INFO", "message": f"Destroyed '{boot_environment_dataset}"}, verbose)
+            {"level": "INFO", "message": f"Destroyed '{destroy_dataset}"}, verbose)
     else:
-        if pyzfscmds.utility.is_clone(boot_environment_dataset):
+        if pyzfscmds.utility.is_clone(destroy_dataset):
             ZELogger.verbose_log({
                 "level": "INFO",
                 "message": (f"Boot environment '{target}' is a clone.\n"
                             "Checking to make sure there are no dependant clones to promote.\n")
             }, verbose)
 
-            # Get origin snapshots
+            # Get and promote snapshots
 
-            origin_all_snaps = None
-            try:
-                origin_all_snaps = pyzfscmds.cmd.zfs_list(
-                    be_pool, recursive=True,
-                    columns=['origin'], zfs_types=['filesystem', 'snapshot', 'volume'])
-            except RuntimeError as e:
-                ZELogger.log({
-                    "level": "EXCEPTION",
-                    "message": f"Failed to list origin snapshots for '{be_pool}'.\n{e}"
-                }, exit_on_error=True)
-
-            split_snaps = zedenv.lib.be.split_zfs_output(origin_all_snaps)
-            origin_snaps = [c[0] for c in split_snaps if c[0] != '-']
-
+            promote_snaps = get_promote_snapshots(be_pool, destroy_dataset)
             ZELogger.verbose_log({
                 "level": "INFO",
-                "message": f"Found origin snapshots for '{be_pool}':\n{origin_snaps}"
+                "message": f"Found snapshots to promote:\n{promote_snaps}"
             }, verbose)
 
+            for ds in promote_snaps:
+                if not noop:
+                    try:
+                        pyzfscmds.cmd.zfs_promote(ds)
+                    except RuntimeError:
+                        ZELogger.log({
+                            "level": "EXCEPTION",
+                            "message": f"Failed to promote {ds}\n{e}\n"
+                        }, exit_on_error=True)
+                ZELogger.verbose_log(
+                    {"level": "INFO", "message": f"Promoted {ds}.\n"}, verbose)
 
+        # Get origin snapshots
+
+            # origin_snaps = get_origin_snapshots(be_pool, destroy_dataset)
+            #
+            # ZELogger.verbose_log({
+            #     "level": "INFO",
+            #     "message": f"Found origin snapshots':\n{origin_snaps}"
+            # }, verbose)
 
 
 @click.command(name="destroy",