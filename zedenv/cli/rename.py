9159904077778e9fa0903978e34029f54a88dc68
John Ramsden <johnramsden@riseup.net>
2018-05-26 23:04:23 -0700

    Add 'rename' command

--- zedenv/cli/rename.py
+++ zedenv/cli/rename.py
@@ -1,9 +1,5 @@
 """List boot environments cli"""
 
-import sys
-
-from typing import Optional
-
 import click
 import pyzfscmds.cmd
 import pyzfscmds.system.agnostic
@@ -16,13 +12,45 @@ from zedenv.lib.logger import ZELogger
 
 @click.command(name="rename",
                help="Rename a boot environment.")
-@click.option('--verbose', '-v',
-              is_flag=True,
-              help="Print verbose output.")
 @click.argument('boot_environment')
 @click.argument('new_boot_environment')
-def cli(boot_environment, new_boot_environment, verbose):
+def cli(boot_environment: str, new_boot_environment: str):
     try:
         zedenv.lib.check.startup_check()
+    except RuntimeError as err:
+        ZELogger.log({"level": "EXCEPTION", "message": err}, exit_on_error=True)
+
+    be_root = zedenv.lib.be.root()
+    old_be_dataset = f"{be_root}/{boot_environment}"
+    new_be_dataset = f"{be_root}/{new_boot_environment}"
+    dataset_mountpoint = pyzfscmds.system.agnostic.dataset_mountpoint(old_be_dataset)
+
+    if pyzfscmds.utility.dataset_exists(new_be_dataset):
+        ZELogger.log({
+            "level": "EXCEPTION",
+            "message": f"Boot environment '{new_boot_environment}' already exists.\n"
+        }, exit_on_error=True)
+
+    if zedenv.lib.be.is_current_boot_environment(boot_environment):
+        ZELogger.log({
+            "level": "EXCEPTION",
+            "message": f"Cannot rename current boot environment '{boot_environment}.\n"
+        }, exit_on_error=True)
+
+    if zedenv.lib.be.is_active_boot_environment(old_be_dataset,
+                                                zedenv.lib.be.dataset_pool(old_be_dataset)):
+        ZELogger.log({
+            "level": "EXCEPTION",
+            "message": f"Cannot rename active boot environment '{boot_environment}.\n"
+        }, exit_on_error=True)
+
+    if dataset_mountpoint:
+        ZELogger.log({
+            "level": "EXCEPTION",
+            "message": f"Dataset is mounted to '{dataset_mountpoint}', unmount and try again\n"
+        }, exit_on_error=True)
+
+    try:
+        pyzfscmds.cmd.zfs_rename(old_be_dataset, new_be_dataset)
     except RuntimeError as err:
         ZELogger.log({"level": "EXCEPTION", "message": err}, exit_on_error=True)
