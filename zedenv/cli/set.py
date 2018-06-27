6d7b1ef8cabb8196bdc028b96b82b2f0245aec2f
John Ramsden <johnramsden@riseup.net>
2018-06-27 16:36:13 -0700

    Add set command
    $ zedenv set -v org.zedenv:bootloader=systemdboot \
                    org.zedenv.grub:bootonzfs=no
    Set 'org.zedenv:bootloader=systemdboot' successfully
    Set 'org.zedenv.grub:bootonzfs=no' successfully

--- zedenv/cli/set.py
+++ zedenv/cli/set.py
@@ -12,64 +12,36 @@ import zedenv.lib.check
 from zedenv.lib.logger import ZELogger
 
 
-def zedenv_get(zedenv_properties: Optional[list],
-               scripting: Optional[bool],
-               recursive: Optional[bool],
-               be_root: str):
+def zedenv_set(verbose: Optional[bool], zedenv_properties: Optional[list], be_root: str):
 
-    property_index = 0
-    columns = ["property", "value"]
+    for prop in zedenv_properties:
+        try:
+            pyzfscmds.cmd.zfs_set(be_root, prop)
+        except RuntimeError:
+            ZELogger.log({
+                "level": "EXCEPTION",
+                "message": f"Failed to set zedenv property '{prop}'\n"
+            }, exit_on_error=True)
 
-    zedenv_props = zedenv_properties if zedenv_properties else ["all"]
-
-    if recursive:
-        property_index = property_index + 1
-        columns.insert(0, "name")
-
-    props = None
-    try:
-        props = pyzfscmds.cmd.zfs_get(be_root,
-                                      properties=zedenv_props,
-                                      scripting=scripting,
-                                      recursive=recursive,
-                                      columns=columns,
-                                      zfs_types=['filesystem'])
-    except RuntimeError as err:
-        ZELogger.log({
-            "level": "EXCEPTION",
-            "message": f"Failed to get zedenv properties\n{err}\n"
-        }, exit_on_error=True)
-
-    prop_list = props.splitlines()
-
-    if not scripting:
-        ZELogger.log({"level": "INFO", "message": prop_list[0]})
-
-    for p in prop_list:
-        split_prop = p.split()
-        if split_prop[property_index].startswith("org.zedenv"):
-            ZELogger.log({"level": "INFO", "message": p})
+        if verbose:
+            ZELogger.verbose_log({
+                "level": "INFO",
+                "message": f"Set '{prop}' successfully"
+            }, verbose)
 
 
-@click.command(name="get",
-               help="Get all boot environment properties that are set.")
-@click.option('--recursive', '-r',
+@click.command(name="set",
+               help="Set boot environment properties.")
+@click.option('--verbose', '-v',
               is_flag=True,
-              help="Recursively get all zedenv properties from all datasets under ROOT.")
-@click.option('--scripting', '-H',
-              is_flag=True,
-              help="Scripting output.")
-@click.argument('zedenv_properties', nargs=-1, required=False)
-def cli(zedenv_properties: Optional[list],
-        scripting: Optional[bool],
-        recursive: Optional[bool]):
+              help="Print verbose output.")
+@click.argument('zedenv_properties', nargs=-1, required=True)
+def cli(verbose: Optional[bool],
+        zedenv_properties: Optional[list]):
 
     try:
         zedenv.lib.check.startup_check()
     except RuntimeError as err:
         ZELogger.log({"level": "EXCEPTION", "message": err}, exit_on_error=True)
 
-    zedenv_get(zedenv_properties,
-               scripting,
-               recursive,
-               zedenv.lib.be.root())
+    zedenv_set(verbose, zedenv_properties, zedenv.lib.be.root())
