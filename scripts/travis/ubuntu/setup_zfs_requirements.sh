021a899275cd3bb9fed642bc9835206a464c9b1a
John Ramsden <johnramsden@riseup.net>
2019-01-01 23:35:02 -0800

    Switch travis to 16.04

---
+++ scripts/travis/ubuntu/setup_zfs_requirements.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+# Travis setup script (16.04
+
+# Install zfs requirements
+add-apt-repository -y ppa:jonathonf/zfs && \
+apt-get -q update && \
+apt-get install -y linux-headers-$(uname -r) && \
+apt-get install -y spl-dkms zfs-dkms zfsutils-linux || exit 1
